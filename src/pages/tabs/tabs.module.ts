5ee460b7335da71ab37e2aa968b3ae5b796d3fb8
Rachel Sobel <rachel.sobel@cornell.edu>
2018-01-23 11:43:33 -0500

    turn tabs component back into page

--- src/pages/tabs/tabs.module.ts
+++ src/pages/tabs/tabs.module.ts
@@ -1,24 +1,23 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 
-// import { CompanyDetailPage } from '../company-detail/company-detail';
 import { CompanyDetailPageModule } from '../company-detail/company-detail.module';
 import { JobListingPageModule } from '../job-listing/job-listing.module';
 import { StudentDetailPageModule } from '../student-detail/student-detail.module';
-import { TabsComponent } from './tabs';
+import { TabsPage } from './tabs';
 
 @NgModule({
   declarations: [
-    TabsComponent,
+    TabsPage,
   ],
   imports: [
     CompanyDetailPageModule,
     JobListingPageModule,
     StudentDetailPageModule,
-    IonicPageModule.forChild(TabsComponent),
+    IonicPageModule.forChild(TabsPage),
   ],
   entryComponents: [
-    TabsComponent,
+    TabsPage,
   ],
 })
 export class TabsComponentModule {}
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { JobListingPage } from '../job-listing/job-listing';
 import { StudentDetailPage } from '../student-detail/student-detail';
 import { CompanyDetailPage } from '../company-detail/company-detail';
@@ -10,12 +10,15 @@ import { CompanyDetailPage } from '../company-detail/company-detail';
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
  */
-
+ @IonicPage({
+   name: 'tabs-page',
+   segment: 'app',
+ })
 @Component({
   selector: 'page-tabs',
   templateUrl: 'tabs.html',
 })
-export class TabsComponent {
+export class TabsPage {
   tab1:any = JobListingPage;
   tab2:any = StudentDetailPage;
   tab3:any = CompanyDetailPage;
