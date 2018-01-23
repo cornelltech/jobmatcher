b0bedec7007a24e259d4b51a1686bd40a1ceb67f
Rachel Sobel <rachel.sobel@cornell.edu>
2018-01-23 11:37:35 -0500

    fucking package management

--- src/pages/tabs/tabs.module.ts
+++ src/pages/tabs/tabs.module.ts
@@ -1,28 +1,23 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 
-import { CompanyDetailPage } from '../company-detail/company-detail';
-import { JobDetailPage } from '../job-detail/job-detail';
-import { JobListingPage } from '../job-listing/job-listing';
-import { StudentDetailPage } from '../student-detail/student-detail';
+// import { CompanyDetailPage } from '../company-detail/company-detail';
+import { CompanyDetailPageModule } from '../company-detail/company-detail.module';
+import { JobListingPageModule } from '../job-listing/job-listing.module';
+import { StudentDetailPageModule } from '../student-detail/student-detail.module';
 import { TabsComponent } from './tabs';
 
 @NgModule({
   declarations: [
-    CompanyDetailPage,
-    JobDetailPage,
-    JobListingPage,
-    StudentDetailPage,
     TabsComponent,
   ],
   imports: [
+    CompanyDetailPageModule,
+    JobListingPageModule,
+    StudentDetailPageModule,
     IonicPageModule.forChild(TabsComponent),
   ],
   entryComponents: [
-    CompanyDetailPage,
-    JobDetailPage,
-    JobListingPage,
-    StudentDetailPage,
     TabsComponent,
   ],
 })
