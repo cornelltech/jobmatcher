8300ab51e709cf172a9911178350d680e1a0d2e1
AVatch <avatchinsky@outlook.com>
2018-01-30 14:36:07 -0500

    created invitation provider and creating invitations w/ firebase

---
+++ src/providers/invitations/invitations.ts
@@ -0,0 +1,37 @@
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/map';
+
+import { Injectable } from '@angular/core';
+
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+
+import { AngularFireDatabase } from 'angularfire2/database';
+
+import { guid } from '../../utilities/helpers';
+import { Invitation } from '../../models/invitation';
+import * as firebase from 'firebase/app';
+
+
+@Injectable()
+export class InvitationsProvider {
+
+    constructor(private db: AngularFireDatabase) { }
+
+    create(target:string, name:string, permissionScope:'recruiter' | 'student'='student', affiliationId:string='Cornell Tech') {
+        const itemRef = this.db.list('invitations');
+        const uuid:string = guid();
+        const isSent:boolean = false;
+        const payload:Invitation = {
+            uuid,
+            target,
+            permissionScope,
+            affiliationId,
+            isSent,
+            name
+        };
+
+        itemRef.push(payload);
+    }
+
+}
