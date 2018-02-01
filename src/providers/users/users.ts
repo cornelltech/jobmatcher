b0d63f12fa8358a298b0955251f8d3eb3eaf3c0b
Rachel Sobel <rachel.sobel@cornell.edu>
2018-02-01 11:26:16 -0500

    modifying favoritedJob logic

--- src/providers/users/users.ts
+++ src/providers/users/users.ts
@@ -36,7 +36,36 @@ export class UsersProvider {
         logo: null
       },
     },
-    jobs: []
+    jobs: [
+      {
+        id: 'stusfdffsd',
+        title: 'something',
+        description: `
+        RegExr v3 was created by gskinner.com, and is proudly hosted by Media Temple.
+
+        Edit the Expression & Text to see matches. Roll over matches or the expression for details. PCRE & Javascript flavors of RegEx are supported.
+
+        The side bar includes a Cheatsheet, full Reference, and Help. You can also Save & Share with the Community, and view patterns you create or favorite in My Patterns.
+
+        Explore results with the Tools below. Replace & List output custom results. Details lists capture groups. Explain describes your expression in plain English.
+
+        `,
+        location: 'nevada',
+        company: {
+          id: 'ct2',
+          name: 'cornell tech2',
+          description: 'yolo',
+          link: null,
+          logo: null
+        },
+        requirements: {
+          id: 'sesodnsdsdsd',
+          visa: 'visa1',
+          balla: true
+        },
+        session: null
+      },
+    ]
   }
 
   list$:Observable<any>;
@@ -147,7 +176,7 @@ export class UsersProvider {
   }
 
   isFavoritedJob(job:Job):boolean {
-    return this.me.jobs.some((payload) => payload == job);
+    return this.me.jobs.some((payload) => payload.id === job.id);
   }
 
 }
