edca2022b8f72f7a409dfe6e4c7b788316e6618b
Rachel Sobel <rachel.sobel@cornell.edu>
2018-03-12 16:26:58 -0400

    switch to new api key (hidden from git), delete old one

--- scripts/upload_data.py
+++ scripts/upload_data.py
@@ -1,8 +1,20 @@
+import os
 import pyrebase
+
+from dotenv import load_dotenv
+from os.path import join, dirname
+
+try:
+    dotenv_path = join(dirname(__file__), '.env')
+    load_dotenv(dotenv_path)
+except Exception as e:
+    print("\nMissing .env file\n")
+
+FIREBASE_API_KEY = os.environ.get('FIREBASE_API_KEY', None)
 
 config = {
     # https://console.developers.google.com/apis/credentials?project=foundry-dev-192623
-    "apiKey": "AIzaSyCDLDGEw6FFj2lJCc7jHA4tal7HIFr1220",
+    "apiKey": FIREBASE_API_KEY,
     "authDomain": "foundry-dev-192623.firebaseapp.com",
     "databaseURL": "https://foundry-dev-192623.firebaseio.com/",
     "storageBucket": "foundry-dev-192623.appspot.com",
@@ -10,11 +22,24 @@ config = {
     "serviceAccount": "service-account-credentials.json"
 }
 
-firebase = pyrebase.initialize_app(config)
+def create_job(title, company_id, description, location, requirements):
+    data = {}
+    data['title'] = title
+    data['company'] = company_id
+    data['description'] = description
+    data['location'] = location
+    data['requirements'] = requirements
+    return data
 
-auth = firebase.auth()
+def add_job(db, job):
+    db.child('jobs').push(job)
 
-# Get a reference to the database service
-db = firebase.database()
-users = db.child("users").get()
-print(users.val())
+if __name__ == '__main__':
+    firebase = pyrebase.initialize_app(config)
+    auth = firebase.auth()
+    db = firebase.database()
+    job = create_job('el prez', '-L5l3TP1ycq9ZMKZd-A8', 'presiding', 'washington, dc', 'visa3')
+    add_job(db, job)
+
+    users = db.child("jobs").get()
+    print(users.val())
